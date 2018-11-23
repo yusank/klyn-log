9e87abd743d9e6053a9ace06290d161240285cb9
yushan <yusankurban@gmail.com>
2018-11-23 14:40:48 +0800

    add log path

--- klyn-log.go
+++ klyn-log.go
@@ -155,7 +155,7 @@ func (kl *KlynLog) getWriteAndWrite(b []byte) (err error) {
 // getIOWriter get log final writer and set for kl.logWriter
 func (kl *KlynLog) getIOWriter() (err error) {
 	day := time.Now().Format("2006-01-02")
-	fileName := fmt.Sprintf("%s-%s.log", kl.config.Prefix, day)
+	fileName := fmt.Sprintf("logFiles/%s-%s.log", kl.config.Prefix, day)
 	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return
