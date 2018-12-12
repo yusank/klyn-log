abf99e6018bb2782eb3910de05dfa45dacf76972
yushan <yusankurban@gmail.com>
2018-12-12 15:30:07 +0800

    add license

--- klyn-log.go
+++ klyn-log.go
@@ -1,3 +1,7 @@
+// Copyright 2018 Yusan Kurban. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package klynlog
 
 import (
@@ -11,33 +15,6 @@ import (
 
 	"git.yusank.space/yusank/klyn-log/consts"
 	"git.yusank.space/yusank/klyn-log/utils"
-)
-
-// Logger provide leveled log
-type Logger interface {
-	Trace(j interface{})
-	Debug(j interface{})
-	Info(j interface{})
-	Warn(j interface{})
-	Error(j interface{})
-	Fatal(j interface{})
-	Any(level int, j interface{})
-	OFF()
-}
-
-const (
-	// LoggerLevelTrace - log trace level
-	LoggerLevelTrace = iota + 1
-	// LoggerLevelDebug - log debug level
-	LoggerLevelDebug
-	// LoggerLevelInfo - log info level
-	LoggerLevelInfo
-	// LoggerLevelWarn - log warn level
-	LoggerLevelWarn
-	// LoggerLevelError - log error level
-	LoggerLevelError
-	// LoggerLevelFatal - log fatal level
-	LoggerLevelFatal
 )
 
 // KlynLog - implement Logger and provide cache
---
+++ logger.go
@@ -0,0 +1,32 @@
+// Copyright 2018 Yusan Kurban. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package log
+
+// Logger provide leveled log
+type Logger interface {
+	Trace(j interface{})
+	Debug(j interface{})
+	Info(j interface{})
+	Warn(j interface{})
+	Error(j interface{})
+	Fatal(j interface{})
+	Any(level int, j interface{})
+	OFF()
+}
+
+const (
+	// LoggerLevelTrace - log trace level
+	LoggerLevelTrace = iota + 1
+	// LoggerLevelDebug - log debug level
+	LoggerLevelDebug
+	// LoggerLevelInfo - log info level
+	LoggerLevelInfo
+	// LoggerLevelWarn - log warn level
+	LoggerLevelWarn
+	// LoggerLevelError - log error level
+	LoggerLevelError
+	// LoggerLevelFatal - log fatal level
+	LoggerLevelFatal
+)
