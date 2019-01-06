7279923b006d56530b17cd52f9a0187ee40f8945
yushan <yusankurban@gmail.com>
2019-01-06 17:55:55 +0800

    test: add bench test
    添加三个模式的压力测试，用于对比和测试功能

--- klyn-log_test.go
+++ klyn-log_test.go
@@ -6,7 +6,12 @@ import (
 )
 
 func TestLog(t *testing.T) {
-	logger := DefaultLogger()
+	l := &LoggerConfig{
+		Prefix:    "KLYN",
+		FlushMode: FlushModeEveryLog,
+	}
+	logger := NewLogger(l)
+	// logger := DefaultLogger()
 
 	go func() {
 		for i := 0; i < 1000; i++ {
@@ -45,5 +50,50 @@ func TestLogFLush(t *testing.T) {
 		})
 
 		time.Sleep(time.Millisecond * 10)
+	}
+}
+
+func BenchmarkNewLoggerMode1(b *testing.B) {
+	l := &LoggerConfig{
+		Prefix:    "KLYN",
+		FlushMode: FlushModeEveryLog,
+	}
+
+	logger := NewLogger(l)
+	for i := 0; i < b.N; i++ {
+		logger.Error(map[string]interface{}{
+			"ip":     "127.0.0.1",
+			"userId": i,
+		})
+	}
+}
+
+func BenchmarkNewLoggerMode2(b *testing.B) {
+	l := &LoggerConfig{
+		Prefix:    "KLYN",
+		FlushMode: FlushModeByDuration,
+	}
+
+	logger := NewLogger(l)
+	for i := 0; i < b.N; i++ {
+		logger.Error(map[string]interface{}{
+			"ip":     "127.0.0.1",
+			"userId": i,
+		})
+	}
+}
+
+func BenchmarkNewLoggerMode3(b *testing.B) {
+	l := &LoggerConfig{
+		Prefix:    "KLYN",
+		FlushMode: FlushModeBySize,
+	}
+
+	logger := NewLogger(l)
+	for i := 0; i < b.N; i++ {
+		logger.Error(map[string]interface{}{
+			"ip":     "127.0.0.1",
+			"userId": i,
+		})
 	}
 }
